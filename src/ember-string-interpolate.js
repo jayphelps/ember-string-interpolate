1b49a93cb986a96869558a4ba8d11dee81ccf3c7
Jay Phelps <hello@jayphelps.com>
2014-06-25 14:12:08 -0700

    4 spaces to 2 for better community support

--- src/ember-string-interpolate.js
+++ src/ember-string-interpolate.js
@@ -6,28 +6,28 @@
  * @license
  */
 (function (Ember) {
-    if (Ember === undefined) throw new Error('Ember.js must be loaded before Ember.String.interpolate');
-    
-    var ENV = window.ENV || {},
-        getPropertyKeys = InterpolatedString.getPropertyKeys;
+  if (Ember === undefined) throw new Error('Ember.js must be loaded before Ember.String.interpolate');
 
-    var emberStringInterpolate = Ember.String.interpolate = function (str, context) {
-        return (new InterpolatedString(str, context)).toString();
+  var ENV = window.ENV || {},
+      getPropertyKeys = InterpolatedString.getPropertyKeys;
+
+  var emberStringInterpolate = Ember.String.interpolate = function (str, context) {
+    return (new InterpolatedString(str, context)).toString();
+  };
+
+  var emberComputedInterpolate = Ember.computed.interpolate = function (str) {
+    var ret = Ember.computed(function () {
+      return emberStringInterpolate(str, this);
+    });
+
+    return ret.property.apply(ret, getPropertyKeys(str));
+  };
+
+  // Honor the Ember convention for extending native prototypes
+  if (ENV.EXTEND_PROTOTYPES || Ember.EXTEND_PROTOTYPES) {
+    String.prototype.interpolate = function () {
+      return emberComputedInterpolate(this);
     };
-
-    var emberComputedInterpolate = Ember.computed.interpolate = function (str) {
-        var ret = Ember.computed(function () {
-            return emberStringInterpolate(str, this);
-        });
-
-        return ret.property.apply(ret, getPropertyKeys(str));
-    };
-
-    // Honor the Ember convention for extending native prototypes
-    if (ENV.EXTEND_PROTOTYPES || Ember.EXTEND_PROTOTYPES) {
-        String.prototype.interpolate = function () {
-            return emberComputedInterpolate(this);
-        };
-    }
+  }
 
 })(this.Ember);
